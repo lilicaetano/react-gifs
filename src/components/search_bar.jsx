793b00ed90d305b392926734298c521fd7a4a001
LilianaCaetano <lili.caetano1@gmail.com>
2020-04-01 16:36:04 +0100

    fix warning

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class SearchBar extends Component {
-  handleUpdate = (event) => {
-    this.props.searchFunction(event.target.value);
-  }
-
-  render() {
-    return (
-      <input
-        type="text"
-        className="form-control form-search"
-        onChange={this.handleUpdate}
-      />
-    );
-  }
+function SearchBar({ searchFunction }) {
+  return (
+    <input
+      type="text"
+      className="form-control form-search"
+      onChange={event => searchFunction(event.target.value)}
+    />
+  );
 }
 
 export default SearchBar;
